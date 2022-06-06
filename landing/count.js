46abe51ba4db9da4995c4aa6b60330cbaa8a1ff7
heechankim <heechan.dev@gmail.com>
2022-06-06 16:53:02 +0000

    Change: host

--- landing/count.js
+++ landing/count.js
@@ -1,16 +1,22 @@
 window.onload = async () => {
 
-let user = await fetch("https://api.autosql.co.kr/landing/user")
+// /landing/user
+// /landing/erd
+// /landing/share
+
+let api_server = "http://ec2-13-125-129-63.ap-northeast-2.compute.amazonaws.com:3000/"
+
+let user = await fetch(api_server + "landing/user")
   .then(response => response.json())
   .catch(error => console.log('error', error));
 document.getElementById('user-count').textContent = user.count;
 
-let erd = await fetch("https://api.autosql.co.kr/landing/erd")
+let erd = await fetch(api_server + "landing/erd")
   .then(response => response.json())
   .catch(error => console.log('error', error));
 document.getElementById('erd-count').textContent = erd.count;
 
-let share = await fetch("https://api.autosql.co.kr/landing/share")
+let share = await fetch(api_server + "landing/share")
   .then(response => response.json())
   .catch(error => console.log('error', error));
 document.getElementById('share-count').textContent = share.count;
